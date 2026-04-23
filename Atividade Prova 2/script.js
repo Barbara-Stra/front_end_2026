53f44def0809e832a18fd71cb43d431e85661a31
Barbara-Stra <babistra1508@gmail.com>
2026-04-23 13:22:15 -0300

    Add files via upload

---
+++ Atividade Prova 2/script.js
@@ -0,0 +1,39 @@
+const card = document.getElementById("card2");
+
+function Proximo() {
+
+    document.getElementById("A").textContent = "Grupo 🅳";
+    document.getElementById("1").textContent = "Estados Unidos";
+    document.getElementById("2").textContent = "Paraguai";
+    document.getElementById("3").textContent = "Austrália";
+    document.getElementById("4").textContent = "Turquia";
+    document.getElementById("texto1").textContent = "Os EUA jogam em casa, vantagem histórica em Copas. Austrália enfrenta frequentemente seleções sul-americanas em torneios.";
+    document.getElementById("B").textContent = "Grupo 🅴";
+    document.getElementById("5").textContent = "Alemanha";
+    document.getElementById("6").textContent = "Equador";
+    document.getElementById("7").textContent = "Costa do Marfin";
+    document.getElementById("8").textContent = "Curaçao";
+    document.getElementById("texto2").textContent = "Alemanha costuma dominar fases de grupos. Equador e Costa do Marfim têm estilos físicos semelhantes.";
+    document.getElementById("C").textContent = "Grupo 🅵";
+    document.getElementById("9").textContent = "Holanda";
+    document.getElementById("10").textContent = "Japão";
+    document.getElementById("11").textContent = "Tunísia";
+    document.getElementById("12").textContent = "Suécia";
+    document.getElementById("texto3").textContent = "Brasil, Marrocos e Escócia já dividiram grupo em 1998. Brasil nunca perdeu para a Escócia em Copas.";
+    
+
+    const elementos = [
+        document.getElementById("1"),
+        document.getElementById("2"),
+        document.getElementById("3"),
+        document.getElementById("4")
+    ];
+        
+      
+    
+}
+
+function add(){
+    card.style.display = "block";
+    
+}
